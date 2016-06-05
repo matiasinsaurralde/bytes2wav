8e5472e8f87274eba5800e6bfbc8da1008c0bc26
Matias Insaurralde <matias@insaurral.de>
2016-06-05 06:42:51 -0400

    Basic tests

---
+++ decoder_test.go
@@ -0,0 +1,31 @@
+package bytes2wav_test
+
+import (
+	"testing"
+
+	"github.com/matiasinsaurralde/bytes2wav"
+)
+
+var TestDecoder bytes2wav.Decoder
+
+func init() {
+	TestDecoder = bytes2wav.NewDecoder(&bytes2wav.Settings{
+    Channels: 1,
+    Bits: 32,
+    Rate: 44100,
+    Filename: "test.wav",
+    SampleLow: 0,
+    SampleHigh: 1,
+  })
+}
+
+func TestSingleCharDecoding( t *testing.T) {
+  testChar := "a"
+  TestEncoder.Encode( []byte(testChar), "")
+	output := TestDecoder.Decode("")
+  str := string(output)
+
+  if str != testChar {
+    t.Fatal("Can't decode single char")
+  }
+}
---
+++ encoder_test.go
@@ -0,0 +1,18 @@
+package bytes2wav_test
+
+import (
+	"github.com/matiasinsaurralde/bytes2wav"
+)
+
+var TestEncoder bytes2wav.Encoder
+
+func init() {
+	TestEncoder = bytes2wav.NewEncoder(&bytes2wav.Settings{
+    Channels: 1,
+    Bits: 32,
+    Rate: 44100,
+    Filename: "test.wav",
+    SampleLow: 0,
+    SampleHigh: 1,
+  })
+}
