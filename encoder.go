f622cd21b4559a997b4516138ab7e066b5a8f86d
Matias Insaurralde <matias@insaurral.de>
2016-06-05 06:11:49 -0400

    The encoder works!

--- encoder.go
+++ encoder.go
@@ -1,68 +1,68 @@
 package bytes2wav
 
-import(
-  "os"
-  "strconv"
+import (
+	"os"
+	"strconv"
 
-  "github.com/cryptix/wav"
+	"github.com/cryptix/wav"
 )
 
-type Encoder interface{
-  Encode([]byte, string)
+type Encoder interface {
+	Encode([]byte, string)
 }
 
-type EncoderSettings struct {
-  Channels uint16
-  Bits uint16
-  Rate uint32
-  OutputFilename string
-  SampleLow int32
-  SampleHigh int32
+func NewEncoder(settings *Settings) Encoder {
+	encoder := Encoder(settings)
+	return encoder
 }
 
-func NewEncoder(settings *EncoderSettings) Encoder {
-  encoder := Encoder(settings)
-  return encoder
+func (e *Settings) Encode(input []byte, output string) {
+	var outputFilename string = output
+
+	if output == "" {
+		outputFilename = e.Filename
+	}
+
+	wavOut, err := os.Create(outputFilename)
+	defer wavOut.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	meta := wav.File{
+		Channels:        e.Channels,
+		SampleRate:      e.Rate,
+		SignificantBits: e.Bits,
+	}
+
+	writer, err := meta.NewWriter(wavOut)
+	defer writer.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	var writeErr error
+
+	for _, v := range input {
+		v := int64(v)
+		b := strconv.FormatInt(v, 2)
+		for _, ch := range b {
+			ch := string(ch)
+			if ch == "0" {
+				writeErr = writer.WriteInt32(e.SampleLow)
+			} else {
+				writeErr = writer.WriteInt32(e.SampleHigh)
+			}
+			checkErr(writeErr)
+		}
+	}
+
 }
 
-func (e *EncoderSettings) Encode(input []byte, output string ) {
-  var outputFilename string = output
-
-  if output == "" {
-    outputFilename = e.OutputFilename
-  }
-
-  wavOut, err := os.Create( outputFilename)
-  defer wavOut.Close()
-
-  if err != nil {
-    panic(err)
-  }
-
-  meta := wav.File{
-    Channels: e.Channels,
-    SampleRate: e.Rate,
-    SignificantBits: e.Bits,
-  }
-
-  writer, err := meta.NewWriter( wavOut)
-
-  if err != nil {
-    panic(err)
-  }
-
-  for _, v := range input {
-    v := int64(v)
-    b := strconv.FormatInt( v, 2 )
-    for _, c := range b {
-      c := string(c)
-      if c == "0" {
-        writer.WriteInt32( e.SampleLow)
-      } else {
-        writer.WriteInt32( e.SampleHigh)
-      }
-    }
-  }
-
-
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
